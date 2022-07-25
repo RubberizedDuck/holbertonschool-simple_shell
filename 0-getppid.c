c90def017526060ff50e65a6c4662e9c36ba8663
RubberizedDuck <dyl_2536@hotmail.com>
2022-07-24 17:55:56 -0700

    Add more tests

--- 0-getppid.c
+++ 0-getppid.c
@@ -2,17 +2,22 @@
 #include <unistd.h>
 
 /**
- * main - returns the parent process ID
+ * main - returns the current and parent process ID
  * Return: will always be successful, returns 0
  */
 
 int main(void)
 {
-	int p_id;
+	pid_t my_pid, parent_pid;
+	unsigned int test;
 
-	p_id = getppid(); /*parent process id*/
+	parent_pid = getppid(); /* parent process id */
+	my_pid = getpid(); /* current process id */
+	test = getpid(); /* testing if id can be stored in integer */
 
-	printf("%u\n", p_id);
+	printf("Current pid: %u\n", my_pid);
+	printf("Testing pid: %u\n", test);
+	printf("Parent pid: %u\n", parent_pid);
 
 	return (0);
 }
