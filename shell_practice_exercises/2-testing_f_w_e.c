8cf9a2671cc5ca8309d19e426df2c609e49321ce
Gotmished <5002@holbertonschool.com>
2022-07-27 00:51:34 -0700

    An edit of Dylan's fork_wait_execve file for simplicity, and to loop five times

---
+++ shell_practice_exercises/2-testing_f_w_e.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+/**
+ * main - fork & wait example
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	pid_t child_pid, father_pid;
+	int status, i;
+	char *av[] = {"/bin/ls", "-l", "/tmp", NULL};
+
+	father_pid = getpid();
+	printf("Before we begin, your father's pid is: %u\n", father_pid);
+	i = 0;
+	while (i < 5)
+	{
+		child_pid = fork();
+		if (child_pid == -1)
+		{
+			perror("Error:");
+			return (1);
+		}
+		if (child_pid == 0)
+		{
+			printf("Wait for me, wait for me!\n");
+			if (execve(av[0], av, NULL) == -1)
+			{
+				perror("Error:");
+			}
+			printf("Dad, my pid is: %u\n", child_pid);
+			sleep(3);
+		}
+		else
+		{
+			wait(&status);
+			printf("Oh, it's all better now! My pid is: %u\n", child_pid);
+		}
+		i = i + 1;
+	}
+	printf("Don't forget, your father's pid is: %u\n", father_pid);
+	return (0);
+}
