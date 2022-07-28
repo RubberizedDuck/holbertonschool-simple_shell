00141fb081285b4cf1e8c26988aff4db5064c082
RubberizedDuck <dyl_2536@hotmail.com>
2022-07-27 22:39:19 -0700

    Add string func protos & linklist func protos

--- main.h
+++ main.h
@@ -12,8 +12,33 @@
 
 extern char **environ;
 
-char *_getenv(const char *name);
+/**
+ * struct list_s - singly linked list
+ * @str: string - (malloc'ed string)
+ * @len: length of the string
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ */
+typedef struct list_s
+{
+	char *str;
+	unsigned int len;
+	struct list_s *next;
+} list_t;
+
+/* string functions */
 int _strlen(char *s);
+char *_strdup(char *str);
+int _strcmp(char *s1, char *s2);
+/* linked list functions */
+list_t *add_node(list_t **head, const char *str);
+list_t *add_node_end(list_t **head, const char *str);
+size_t print_list(const list_t *head);
+void free_list(list_t *head);
+size_t list_len(const list_t *head);
+/* (insert name of funcs) */
+char *_getenv(const char *name);
 void print_env(char *env_name);
 int _env(void);
 void fork_exec(char **argv);
