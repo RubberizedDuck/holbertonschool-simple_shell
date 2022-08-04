b8b68d8fa3f61b9443a69a500abe29d4a1f4e322
Gotmished <5002@holbertonschool.com>
2022-08-03 21:22:02 -0700

    Now storing path tokens in a linked list

---
+++ _getpath.c
@@ -0,0 +1,51 @@
+#include "main.h"
+
+/**
+ * _getpath - builds a linked list of
+ * folders in the PATH, adding nodes at the end
+ * @name: Will be PATH for our purposes, but can be any
+ * environment variable.
+ * Return: 0 upon success
+ */
+
+char **_getpath(char * const envp[])
+{
+	char *str, *token;
+	char **dirs;
+	list_t *node, *head;
+	int listlength, count;
+
+	head = NULL;
+	str = _getenv(envp, "PATH");
+	printf("%s\n", str);
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	token = strtok(str, "=");
+	token = strtok(NULL, ":");
+	while (token != NULL)
+	{
+		add_node_end(&head, token);
+		token = strtok(NULL, ":");
+	}
+	listlength = list_len(head);
+	dirs = malloc(sizeof(*dirs) * (listlength + 1));
+	if (dirs == NULL)
+	{
+		return (NULL);
+	}
+
+	count = 0;
+	node = head;
+	while (count < listlength)
+	{
+		dirs[count] = _strdup(node->str);
+		count = count + 1;
+		node = node->next;
+	}
+	free(token);
+	dirs[count] = NULL;
+	free_list(head);
+	return (dirs);
+}
