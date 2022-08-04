dbc29ed80a588ccd1337f1117efbab3a3b097f41
Gotmished <5002@holbertonschool.com>
2022-08-03 21:25:22 -0700

    added auxiliary functions

--- string_funcs.c
+++ string_funcs.c
@@ -1,12 +1,51 @@
 #include "main.h"
 
 /**
+ * _strcmp - compares two strings
+ * @s1: string one
+ * @s2: string two
+ * @n: specified number of bytes
+ * Return: 0 if the strings match for the specified number of bytes
+ * -1, otherwise
+ */
+
+int _strncmp(const char *s1, const char *s2, int n)
+{
+	int i;
+
+	if (n == 0)
+	{
+		return (-1);
+	}
+
+	i = 0;
+	while (s1[i] != '\0' || s2[i] != '\0')
+	{
+		if (i == n)
+		{
+			return (0);
+		}
+
+		if (s1[i] != s2[i])
+		{
+			return (-1);
+		}
+		i = i + 1;
+	}
+	if (s1[i] == '\0' && s2[i] == '\0')
+	{
+		return (0);
+	}
+	return (-1);
+}
+
+/**
  * _strlen - is a function that returns the length of a string.
  * @s: is the character being passed.
  * Return: len is the string length.
  */
 
-int _strlen(char *s)
+int _strlen(const char *s)
 {
 	int len;
 
@@ -82,4 +121,58 @@ int _strcmp(char *s1, char *s2)
 	/* prints the difference of string 1 and string 2 */
 	ret = s1[i] - s2[i];
 	return (ret);
+}
+
+/**
+ * *_strcat - concatenates two strings
+ * @src: string to appended
+ * @dest: string to append to
+ * Return: pointer to dest
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int i, j;
+
+	i = 0;
+	while (dest[i] != '\0')
+	{
+		i = i + 1;
+	}
+
+	j = 0;
+	while (src[j] != '\0')
+	{
+		dest[i] = src[j];
+		j = j + 1;
+		i = i + 1;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
+
+/**
+ * *_strcpy - copies a string to a buffer
+ * @dest: points to buffer location
+ * @src: points to string location
+ * Return: pointer to dest
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int i, length;
+
+	length = 0;
+	while (src[length] != '\0')
+	{
+		length = length + 1;
+	}
+
+	i = 0;
+	while (i <= length)
+	{
+		dest[i] = src[i];
+		i = i + 1;
+	}
+	return (dest);
 }
