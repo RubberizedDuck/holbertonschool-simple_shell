68391ad65907a3060b16c60f0317567fcee081bc
RubberizedDuck <dyl_2536@hotmail.com>
2022-08-04 20:40:21 -0700

    Remove comment out code

--- simple_shell.c
+++ simple_shell.c
@@ -87,22 +87,10 @@ int _getline(char * const envp[])
 			count = 0;
 			if (args[count] != NULL)
 				free_tab(args);
-			/*while (args[count] != NULL)
-			{
-				free(args[count]);
-				count = count + 1;
-			}
-			free(args);*/
 		}
 	}
 	if (dirs[i] != NULL)
 		free_tab(dirs);
-	/*while (dirs[i] != NULL)
-	{
-		free(dirs[i]);
-		i++;
-	}
-	free(dirs);*/
 	free(line);
 	return (0);
 }
