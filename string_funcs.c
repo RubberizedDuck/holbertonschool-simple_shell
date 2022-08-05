a45e7cbbcf5bb33854485de368959032db53640e
RubberizedDuck <dyl_2536@hotmail.com>
2022-08-04 21:03:33 -0700

    Remove _strcmp

--- string_funcs.c
+++ string_funcs.c
@@ -90,40 +90,6 @@ char *_strdup(char *str)
 }
 
 /**
- * _strcmp - Function to print the difference between strings
- * @s1: First string
- * @s2: Second string
- * Return: ret, which equals the difference of s1 & s2.
- */
-
-int _strcmp(char *s1, char *s2)
-{
-	int i = 0;
-	int ret = 0;
-
-	/* loops if string 1 is equal to string 2 */
-	while (s1[i] == s2[i])
-	{
-		/* if string 1 equals to NULL, loop breaks */
-		if (s1[i] == '\0')
-		{
-			break;
-		}
-		/* if string 2 equals to NULL, loop breaks */
-		if (s2[i] == '\0')
-		{
-			break;
-		}
-		/* else increment i */
-		else
-		i = i + 1;
-	}
-	/* prints the difference of string 1 and string 2 */
-	ret = s1[i] - s2[i];
-	return (ret);
-}
-
-/**
  * *_strcat - concatenates two strings
  * @src: string to appended
  * @dest: string to append to
